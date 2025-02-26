8a822ee2b0f39e213de224a8df33ad4f44398428
pomelohoho <thisispomelodao@gmail.com>
2025-02-26 02:22:59 -0600

    Commit

--- src/data.js
+++ src/data.js
@@ -2,156 +2,154 @@ export const albums = [
     {
       id: 1,
       title: "Lunar New Year",
-      thumbnail: "https://drive.google.com/drive/folders/1J_y0ckLjCALr3s3Qs4Reshy4S4_Bug0K?usp=sharing",
-      images: ["https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album1/LunarNewYear2025.JPG", 
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album1/LunarNewYear2025-2.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album1/LunarNewYear2025-1.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album1/LunarNewYear2025-3.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album1/LunarNewYear2025-4.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album1/LunarNewYear2025-5.JPG"
-      ]
+      thumbnail: "images/album1/LunarNewYear2025.JPG",
+      images: ["images/album1/LunarNewYear2025.JPG", 
+        "images/album1/LunarNewYear2025-1.JPG",
+        "images/album1/LunarNewYear2025-2.JPG",
+        "images/album1/LunarNewYear2025-3.JPG",
+        "images/album1/LunarNewYear2025-4.JPG",
+        "images/album1/LunarNewYear2025-5.JPG"]
     },
     {
       id: 2,
       title: "Hanoi by Night",
-      thumbnail: "https://1drv.ms/i/c/6bd65025459b6028/Ebd1jffMnopFmmfIGcGb04gBCz88hk5bH447A3ICUjEcrQ?e=XPrM5K",
-      images: ["https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album2/HanoiNight.JPG", 
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album2/HanoiNIght-1.JPG", 
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album2/HanoiNight-2.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album2/HanoiNight-3.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album2/HanoiNIght-4.JPG",
-        "/https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album2/HanoiNight-5.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album2/HanoiNIght-6.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album2/HanoiNIght-7.JPG",]
+      thumbnail: "images/album2/HanoiNight.JPG",
+      images: ["images/album2/HanoiNight.JPG", 
+        "images/album2/HanoiNight-1.JPG", 
+        "images/album2/HanoiNight-2.JPG",
+        "images/album2/HanoiNight-3.JPG",
+        "images/album2/HanoiNight-4.JPG",
+        "images/album2/HanoiNight-5.JPG",
+        "images/album2/HanoiNight-6.JPG",
+        "images/album2/HanoiNight-7.JPG",]
     },
     {
       id: 3,
       title: "Flores",
-      thumbnail: "images/album1/LunarNewYear2025-1.JPG",
-      images: ["https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album3/Flores.JPG", 
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album3/Flores-1.jpg",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album3/Flores-2.jpg",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album3/Flores-3.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album3/Flores-4.jpg",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album3/Flores-5.jpg",
+      thumbnail: "images/album3/Flores.JPG",
+      images: ["images/album3/Flores.JPG", 
+        "images/album3/Flores-1.jpg",
+        "images/album3/Flores-2.jpg",
+        "images/album3/Flores-3.JPG",
+        "images/album3/Flores-4.jpg",
+        "images/album3/Flores-5.jpg",
       ]
     },
     {
       id: 4,
       title: "Jeans&Roses",
-      thumbnail: "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album4/JeanRoses.JPG",
-      images: ["https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album4/JeanRoses.JPG", 
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album4/JeanRoses-1.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album4/JeanRoses-2.PNG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album4/JeanRoses-3.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album4/JeanRoses-4.PNG"]
+      thumbnail: "images/album4/JeanRoses.JPG",
+      images: ["images/album4/JeanRoses.JPG", 
+        "images/album4/JeanRoses-1.JPG",
+        "images/album4/JeanRoses-2.PNG",
+        "images/album4/JeanRoses-3.JPG",
+        "images/album4/JeanRoses-4.PNG"]
     },
     {
       id: 5,
       title: "Scarlet Dream",
-      thumbnail: "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album5/ScarletDream.JPG",
-      images: ["https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album5/ScarletDream.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album5/ScarletDream-1.jpeg",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album5/ScarletDream-2.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album5/ScarletDream-3.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album5/ScarletDream-4.JPG"
+      thumbnail: "images/album5/ScarletDream.JPG",
+      images: ["images/album5/ScarletDream.JPG",
+        "images/album5/ScarletDream-1.jpeg",
+        "images/album5/ScarletDream-2.JPG",
+        "images/album5/ScarletDream-3.JPG",
+        "images/album5/ScarletDream-4.JPG"
        ]
     },
     {
       id: 6,
       title: "Winter Waltz",
-      thumbnail: "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album6/WinterWaltz2024.JPG",
-      images: ["https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album6/WinterWaltz2024.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album6/WinterWaltz2024-1.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album6/WinterWaltz2024-2.jpg",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album6/WinterWaltz2024-3.jpg",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album6/WinterWaltz2024-4.JPG"
+      thumbnail: "images/album6/WinterWaltz2024.JPG",
+      images: ["images/album6/WinterWaltz2024.JPG",
+        "images/album6/WinterWaltz2024-1.jpg",
+        "images/album6/WinterWaltz2024-2.jpg",
+        "images/album6/WinterWaltz2024-3.JPG",
+        "images/album6/WinterWaltz2024-4.JPG"
       ]
     },
     {
       id: 7,
       title: "Picturesque",
-      thumbnail: "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album7/Picturesque.JPG",
-      images: ["https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album7/Picturesque.JPG", 
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album7/Picturesque-1.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album7/Picturesque-2.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album7/Picturesque-3.JPG"]
+      thumbnail: "images/album7/Picturesque.JPG",
+      images: ["images/album7/Picturesque.JPG", 
+        "images/album7/Picturesque-1.JPG",
+        "images/album7/Picturesque-2.JPG",
+        "images/album7/Picturesque-3.JPG"]
     },
     {
       id: 8,
       title: "Winter Waltz",
-      thumbnail: "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album8/WinterWaltz2025.jpg",
-      images: ["https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album8/WinterWaltz2025.jpg",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album8/WinterWaltz2025-1.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album8/WinterWaltz2025-2.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album8/WinterWaltz2025-3.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album8/WinterWaltz2025-4.JPG"
+      thumbnail: "images/album8/WinterWaltz2025.jpg",
+      images: ["images/album8/WinterWaltz2025.jpg",
+        "images/album8/WinterWaltz2025-1.JPG",
+        "images/album8/WinterWaltz2025-2.JPG",
+        "images/album8/WinterWaltz2025-3.JPG",
+        "images/album8/WinterWaltz2025-4.JPG"
       ]
     },
     {
       id: 9,
       title: "Yearbook",
-      thumbnail: "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album9/UrbanNightfall.JPEG",
-      images: ["https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album9/UrbanNightfall.JPEG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album9/UrbanNightfall-1.JPEG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album9/UrbanNightfall-2.JPEG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album9/UrbanNightfall-3.JPEG"
+      thumbnail: "images/album9/UrbanNightfall.JPEG",
+      images: ["images/album9/UrbanNightfall.JPEG",
+        "images/album9/UrbanNightfall-1.JPEG",
+        "images/album9/UrbanNightfall-2.JPEG",
+        "images/album9/UrbanNightfall-3.JPEG"
       ]
     },
     {
       id: 10,
       title: "Playground",
-      thumbnail: "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album10/Playground.JPG",
-      images: ["https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album10/Playground.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album10/Playground-1.jpg",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album10/Playground-2.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album10/Playground-3.jpg",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album10/Playground-4.JPG"
+      thumbnail: "images/album10/Playground.JPG",
+      images: ["images/album10/Playground.JPG",
+        "images/album10/Playground-1.jpg",
+        "images/album10/Playground-2.JPG",
+        "images/album10/Playground-3.jpg",
+        "images/album10/Playground-4.JPG"
       ]
     },
     {
       id: 11,
       title: "Dance",
-      thumbnail: "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album11/Dance.JPG",
-      images: ["https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album11/Dance.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album11/Dance-1.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album11/Dance-2.jpg"
+      thumbnail: "images/album11/Dance.JPG",
+      images: ["images/album11/Dance.JPG",
+        "images/album11/Dance-1.jpg",
+        "images/album11/Dance-2.JPG"
       ]
     },
     {
       id: 12,
       title: "Ao Dai in Grinnell",
-      thumbnail: "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album12/AoDai.jpg",
-      images: ["https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album12/AoDai.jpg",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album12/AoDai-1.jpg",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album12/AoDai-2.jpg",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album12/AoDai-3.jpg"
+      thumbnail: "images/album12/AoDai.jpg",
+      images: ["images/album12/AoDai.jpg",
+        "images/album12/AoDai-1.jpg",
+        "images/album12/AoDai-2.jpg",
+        "images/album12/AoDai-3.jpg"
       ]
     },
     {
       id: 13,
       title: "Lunar New Year",
-      thumbnail: "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album13/LunarNewYear2024.JPG",
-      images: ["https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album13/LunarNewYear2024.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album13/LunarNewYear2024-1.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album13/LunarNewYear2024-2.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album13/LunarNewYear2024-4.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album13/LunarNewYear2024-5.JPG"
+      thumbnail: "images/album13/LunarNewYear2024.JPG",
+      images: ["images/album13/LunarNewYear2024.JPG",
+        "images/album13/LunarNewYear2024-2.JPG",
+        "images/album13/LunarNewYear2024-4.JPG",
+        "images/album13/LunarNewYear2024-5.JPG",
       ]
     },
     {
       id: 14,
       title: "Halloween",
-      thumbnail: "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album14/Halloween.JPG",
-      images: ["https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album14/Halloween.JPG",
-        "https://github.com/pomelohoho/Model-Portfolio/blob/b309dab97a9d3e5791a203305e7a1e30e43a537b/images/album14/Halloween-1.JPG"
+      thumbnail: "images/album14/Halloween.JPG",
+      images: ["images/album14/Halloween.JPG",
+        "images/album14/Halloween-1.JPG"
       ]
     }]
     // {
     //   id: 15,
     //   title: "10/10",
-    //   thumbnail: "/images/album15/1010.jpg",
-    //   images: ["/images/album15/1010.jpg",
-    //      "C:/Users/Admin/fashion-portfolio/public/images/album15/1010-01.JPG"
+    //   thumbnail: "images/album15/1010.jpg",
+    //   images: ["images/album15/1010.jpg",
+    //      "C:/Users/Admin/fashion-portfolio/publicimages/album15/1010-01.JPG"
     //   ]
     // }]

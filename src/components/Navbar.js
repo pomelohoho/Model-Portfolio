15fa7a7977428988d7b6345071bd60ee2ccf4aee
pomelohoho <thisispomelodao@gmail.com>
2025-02-26 02:25:26 -0600

    Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,11 @@ export default function Navbar() {
   return (
     <header className="navbar">
       <div className="navbar-left">
-        <h1>POM DAO</h1>
+        <h1>POM</h1>
       </div>
       <nav className="navbar-right">
         {/* Replace with your actual routes/links */}
-        <Link to="/about">ABOUT INFO</Link>
+        <Link to="/about">ABOUT</Link>
         <Link to="/contact">CONTACT</Link>
         <Link to="/">HOME</Link>
         <Link to="/gallery">GALLERY</Link>
